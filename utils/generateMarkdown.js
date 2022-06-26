3a46efe6ab3df8b683fde4a33f37a226a60d9d65
Your Name <you@example.com>
2022-06-25 20:51:49 -0400

    Update user prompts

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license==='MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  if (license==='APACHE 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  if (license==='GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  if (license==='BSD 3') {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,9 +27,12 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return (
   `# ${data.title}
+
   ${renderLicenseBadge(data.license)}
+
   ## Project Description
-  ${data.desc}
+  ${data.description}
+
   ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
@@ -24,19 +40,25 @@ function generateMarkdown(data) {
   - [Contributing](#contributing)
   - [Tests](#tests)
   - [Questions](#questions)
+
   ## Installation
   ${data.install}
+
   ## Usage
   ${data.usage}
+
   ## License
-  This application is covered under ${data.license}
+  This repository is licensed under the ${data.license} license.
+
   ## Contributing
   ${data.contributing}
+
   ## Tests
+  To run tests, run the following:
   ${data.tests}
+
   ## Questions
-  - Github: ${data.userName} https://github.com/${data.userName}
-  - If you have any questions, please email ${data.email}
+  Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work in GitHub at [${data.userName}](https://github.com/${data.userName}
   `);
 };
 module.exports = generateMarkdown;
